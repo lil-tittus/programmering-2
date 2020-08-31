f2262e1b28ace61d9d8bf3de806c6277584bb1b6
lil-tittus <jared.tittus@elev.ga.ntig.se>
2020-08-31 13:41:18 +0200

    klar niklas

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -6,7 +6,30 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Ditt användarnamn:");
+
+           
+            string userName = Console.ReadLine();
+
+           
+            Console.WriteLine("Ditt namn är: " + userName);
+
+            Console.WriteLine("Din ålder:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Din ålder är: " + age);
+
+            Console.WriteLine("Lever du? ja eller nej");
+
+            var svar = Console.ReadLine();
+
+            if (svar == "ja")
+            {
+                Console.WriteLine("Bra pee pee poo poo");
+            }
+            else
+            {
+                Console.WriteLine("stinky pope");
+            }
         }
     }
 }
