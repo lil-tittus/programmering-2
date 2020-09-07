ff8b33ee45fde249ec14b4a3e2c2c5ca4910f987
lil-tittus <jared.tittus@elev.ga.ntig.se>
2020-09-07 13:22:24 +0200

    klar

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new String[5];
+
+            Console.WriteLine("Type 5 names aqnd yeehaa");
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                name[i] = Console.ReadLine();
+            }
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+            
+           
+            
+            
+        }
+    }
+}
