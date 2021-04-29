7a1ebc9cd2e2284c87b2e71e173558acb1ed3cb3
lil-tittus <54933526+lil-tittus@users.noreply.github.com>
2021-04-29 13:05:15 +0200

    Gjort så att bakgrunden aldrig tar slut

--- Slutprojekt/slutprojekt/slutprojekt/MainWindow.xaml.cs
+++ Slutprojekt/slutprojekt/slutprojekt/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace slutprojekt
 {
     /// <summary>
@@ -20,9 +22,146 @@ namespace slutprojekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        Rect playerHitBox;
+        Rect groundHitBox;
+        Rect obstacleHitBox;
+
+        bool jumping;
+
+        int force = 20;
+        int speed = 5;
+
+        Random rnd = new Random();
+
+        bool gameOver;
+
+        double spriteIndex = 0;
+
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgroundSprite = new ImageBrush();
+        ImageBrush obstacleSprite = new ImageBrush();
+
+        int[] obstaclePosition = { 320, 310, 300, 305, 315 };
+
+        int score = 0; 
         public MainWindow()
         {
             InitializeComponent();
+
+            MyCanvas.Focus();
+
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            backgroundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./background.gif"));
+
+            background.Fill = backgroundSprite;
+            background2.Fill = backgroundSprite;
+
+            StartGame();
+
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(background, Canvas.GetLeft(background) - 18);
+            Canvas.SetLeft(background2, Canvas.GetLeft(background2) - 18);
+
+            if(Canvas.GetLeft(background) < -1262)
+            {
+                Canvas.SetLeft(background, Canvas.GetLeft(background2) + background2.Width);
+            }
+
+            if (Canvas.GetLeft(background2) < -1262)
+            {
+                Canvas.SetLeft(background2, Canvas.GetLeft(background) + background.Width);
+            }
+
+            Canvas.SetTop(player, Canvas.GetTop(player) - speed);
+            Canvas.SetLeft(obstacle, Canvas.GetLeft(obstacle) - 12);
+
+            ScoreText.Content = "Score: " + score;
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter && gameOver == true)
+            {
+                StartGame();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space && jumping == false && Canvas.GetTop(player) > 261)
+            {
+                jumping = true;
+                force = 15;
+                speed = -12;
+
+                playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_02.gif"));
+            }
+        }
+
+        private void StartGame()
+        {
+            Canvas.SetLeft(background, 0);
+            Canvas.SetLeft(background2, 1262);
+
+            Canvas.SetLeft(player, 110);
+            Canvas.SetTop(player, 140);
+
+            Canvas.SetLeft(obstacle, 950);
+            Canvas.SetTop(obstacle, 310);
+
+            RunSprite(1);
+
+            obstacleSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./obstacle.png"));
+            obstacle.Fill = obstacleSprite;
+
+            jumping = false;
+            gameOver = false;
+            score = 0;
+
+            ScoreText.Content = "Score: " + score;
+
+            gameTimer.Start();
+        }
+
+        private void RunSprite(double i)
+        {
+            switch (i)
+            {
+                case 1:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_01.gif"));
+                    break;
+                case 2:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_02.gif"));
+                    break;
+                case 3:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_03.gif"));
+                    break;
+                case 4:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_04.gif"));
+                    break;
+                case 5:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_05.gif"));
+                    break;
+                case 6:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_06.gif"));
+                    break;
+                case 7:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_07.gif"));
+                    break;
+                case 8:
+                    playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Slutprojekt./newRunner_08.gif"));
+                    break;
+            }
+
+            player.Fill = playerSprite;
         }
     }
 }
