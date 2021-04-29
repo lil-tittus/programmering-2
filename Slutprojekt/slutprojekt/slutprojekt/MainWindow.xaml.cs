c02f21bb990a10ebc00ae8c2be59021715d0ec10
lil-tittus <54933526+lil-tittus@users.noreply.github.com>
2021-04-29 13:44:23 +0200

    klar med all kod har bara inte listat ut hur jag ska lösa med bilderna

--- Slutprojekt/slutprojekt/slutprojekt/MainWindow.xaml.cs
+++ Slutprojekt/slutprojekt/slutprojekt/MainWindow.xaml.cs
@@ -66,8 +66,8 @@ namespace slutprojekt
 
         private void GameEngine(object sender, EventArgs e)
         {
-            Canvas.SetLeft(background, Canvas.GetLeft(background) - 18);
-            Canvas.SetLeft(background2, Canvas.GetLeft(background2) - 18);
+            Canvas.SetLeft(background, Canvas.GetLeft(background) - 3);
+            Canvas.SetLeft(background2, Canvas.GetLeft(background2) - 3);
 
             if(Canvas.GetLeft(background) < -1262)
             {
@@ -79,10 +79,80 @@ namespace slutprojekt
                 Canvas.SetLeft(background2, Canvas.GetLeft(background) + background.Width);
             }
 
-            Canvas.SetTop(player, Canvas.GetTop(player) - speed);
+            Canvas.SetTop(player, Canvas.GetTop(player) + speed);
             Canvas.SetLeft(obstacle, Canvas.GetLeft(obstacle) - 12);
 
             ScoreText.Content = "Score: " + score;
+
+            playerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width - 15, player.Height);
+            obstacleHitBox = new Rect(Canvas.GetLeft(obstacle), Canvas.GetTop(obstacle), obstacle.Width, obstacle.Height);
+            groundHitBox = new Rect(Canvas.GetLeft(ground), Canvas.GetTop(ground), ground.Width, ground.Height);
+
+            if (playerHitBox.IntersectsWith(groundHitBox))
+            {
+                speed = 0;
+
+                Canvas.SetTop(player, Canvas.GetTop(ground) - player.Height);
+
+                jumping = false;
+
+                spriteIndex += .5;
+
+                if(spriteIndex > 8)
+                {
+                    spriteIndex = 1;
+                }
+                RunSprite(spriteIndex);
+            }
+
+            if(jumping == true)
+            {
+                speed = -9;
+
+                force -= 1;
+            }
+            else
+            {
+                speed = 12;
+            }
+
+            if(force < 0)
+            {
+                jumping = false;
+            }
+
+            if(Canvas.GetLeft(obstacle) < -50)
+            {
+                Canvas.SetLeft(obstacle, 950);
+
+                Canvas.SetTop(obstacle, obstaclePosition[rnd.Next(0, obstaclePosition.Length)]);
+
+                score += 1;
+            }
+
+            if (playerHitBox.IntersectsWith(obstacleHitBox))
+            {
+                gameOver = true;
+
+                gameTimer.Stop();
+            }
+
+            if(gameOver == true)
+            {
+                obstacle.Stroke = Brushes.Black;
+                obstacle.StrokeThickness = 1;
+
+                player.Stroke = Brushes.Red;
+                player.StrokeThickness = 1;
+
+                ScoreText.Content = "Score: " + score + "Press Enter to play again";
+            }
+            else
+            {
+                player.StrokeThickness = 0;
+                obstacle.StrokeThickness = 0;
+            }
+
 
         }
 
